d881e1eaa35632d6a53c4afef11e1520c16beb0f
mofya1 <mofya@surveycto.com>
2020-04-30 11:08:59 +0200

    added saving answer and comments so code

--- source/script.js
+++ source/script.js
@@ -1,13 +1,14 @@
-var choices = fieldProperties.CHOICES;
-var numberOfChoices = choices.length;
-var fieldType = fieldProperties.FIELDTYPE;
+var choices = fieldProperties.CHOICES; // Get the full list of choices available
+var numberOfChoices = choices.length; // Count the number of choices for this field
+var fieldType = fieldProperties.FIELDTYPE; // Get the type of field (select_one or select_multiple)
 var result;
-var delimiter = getPluginParameter ("delimiter");
-var headers = getPluginParameter("columns");
-var headerArray = headers.split(",");
+var delimiter = getPluginParameter ("delimiter");  // Get thd parameter for the delimiter entered in the form definition
+var headers = getPluginParameter("headers"); // Get the list of headers entered in the form definition
+var headerArray = headers.split(","); // get individual headers from the headers parameter 
 
-var style = 'single';
+var style = 'single'; // set the default style to select one
 
+// check if the field is multi select and set selection to allow selecting multiple fields
 if (fieldType === 'select_one'){
   style = 'single';
 }
@@ -15,8 +16,9 @@ else {
   style = 'multi';
 }
 
-var data = [];
+var data = []; // Array to keep the data for the data table
 
+// Loop through the choices and create key value pairs
 for(let i = 0; i < numberOfChoices; i++ ){
   const item = {};
   const choicesLabel = choices[i].CHOICE_LABEL;
@@ -32,99 +34,88 @@ for(let i = 0; i < numberOfChoices; i++ ){
   data.push(item);
 }
 
-//var column;
-
-console.log(data[0]);
-
+// Create a placeholder for the table
 var $thead = $('#tableId').find('thead');
 var tr = $("<tr>");
 $thead.append(tr);
-var columns = [];
+var columns = []; // Array to store the column headers
+
+// Loop through the first and designate these as column headers
 $.each(data[0], function(name, value) {
   var column  = {
     "data": name,
     "title":name
   };
-  // if(name === 0){
-  //   column = {
-  //     "data": name,
-  //     "title":name,
-  //     "className": 'select-checkbox'
-  //   }
-  // }else{
-  //   column = {
-  //     "data": name,
-  //     "title":name
-  //   }
-  // }
   columns.push(column);
 });
 
+// Using datatables create a table dynamically
 let table = $('#tableId').DataTable({
-  data: data,
-  columns: columns,
-  columnDefs:[
+  data: data, // data for each row
+  columns: columns, // column headers for each column
+  columnDefs:[ // Make specific alterations to certain columns of the table
     {
-      targets : 0,
+      targets : 0, // targets the first column
       searchable : false,
       sortable : false,
       orderable : false,
-      className: 'select-checkbox',
+      className: 'select-checkbox', // adds checkboxes to the first column
       title : " "
     },
     {
-    targets : 1,
-    visible : false,
+    targets : 1, // targets the second column
+    visible : false, // Use this to hide the value from the table
     searchable : false,
     sortable : false,
     orderable : false
   }],
-  order : [],
-  info : false,
-  paging : false,
-  select: {
-    items : 'row',
-    style : style
+  order : [], // Remove the sorting icon from the first column. Also allows for default sorting to be used
+  info : false, // Hide table information
+  paging : false, // Remove table paging
+  select: { // Allow section for this table
+    items : 'row', // Allow selection by row
+    style : style // Allow selection based on fieldType (single/multi)
   },
   fixedHeader : true,
   drawCallback : function(settings){
-    $('#tableId tr:eq(0) th:eq(0)').text(" ");
+    $('#tableId tr:eq(0) th:eq(0)').text(" "); // Set header for first column to blank
   }
 });
 
-getSelected();
+getSelected(); // Call method to process selections
 
-let ids;
+let ids; // Variable to keep track of the selected rows
 
-function getSelected(){
-  table.on( 'select', function ( e, dt, type, indexes ) {//process what is selected
+function getSelected(){ 
+  table.on( 'select', function ( e, dt, type, indexes ) { // On row selection
     if ( type === 'row' ) {
       ids = $.map(table.rows({ selected: true }).data(),function(item){
-          return item.value;
+          return item.value; // save the value of the row
       });
     }
 
-    setValue(ids);
+    setValue(ids); // Pass ids to setValue
   });
   table.on( 'deselect', function ( e, dt, type, indexes ) {
     if ( type === 'row' ) {
-        ids = $.map(table.rows({ selected: true }).data(),function(item){
-            return item.value;
+        ids = $.map(table.rows({ selected: true }).data(),function(item){ // On row deselection
+            return item.value; // remove the value of the deselected row
         });
     }
 
-    setValue(ids);
+    setValue(ids); // Pass ids to setValue
     });
 }
 
+// Sets the value of selection based on selected rows
 function setValue(value){
-  if(fieldType === 'select_multiple' && value.length > 1){
-    const answer = value.join(" ");
-    setAnswer(answer);
+  if(fieldType === 'select_multiple' && value.length > 1){ // check if to expect more than one selection
+    const answer = value.join(" "); // Change array into space seperated string
+    setAnswer(answer); // Set this as the answer for this field
   }else{
-    if(value.length === 1){
-      const answer = value.toString(value);
-      setAnswer(answer);
+    if(value.length === 1){ // Check if only one answer is checked
+      const answer = value.toString(value); // Change a array into a string value
+      setAnswer(answer); // Set this as the answer for the field
     }
   }
 }
